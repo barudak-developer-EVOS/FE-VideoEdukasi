982b0c508caa5e369a2b11bc96d247e0ea6447ef
Sahl Hafidz <akunbelajarhafidz@gmail.com>
2024-12-07 10:09:32 +0700

    feat(layout):slicing design login page csr

---
+++ pages/login/index.tsx
@@ -0,0 +1,6 @@
+import dynamic from 'next/dynamic';
+
+const Component = dynamic(() => import(/* webpackChunkName: "login-page" */ '../../src/components/templates/Login'), {
+  ssr: false,
+});
+export default Component;
