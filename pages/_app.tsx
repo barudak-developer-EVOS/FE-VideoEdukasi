85b87eccf8ef60a5517b785dcebd8cda5c314d09
Sahl Hafidz <akunbelajarhafidz@gmail.com>
2024-12-23 19:06:57 +0700

    feat(layot):enter logo eduvid

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import type { AppProps } from "next/app";
 import Layout from "@/components/molecules/Layout";
 import { useRouter } from "next/router";
+import "../src/styles/globals.css";
 const IGNORED_LAYOUT = ["/login"];
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
