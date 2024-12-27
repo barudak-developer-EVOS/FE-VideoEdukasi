6371586d977f2c45dafeba6a3b47e88e9c6cd173
Sahl Hafidz <akunbelajarhafidz@gmail.com>
2024-12-27 20:29:53 +0700

    fix(layout):reset from zero

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,17 +3,23 @@ import React from "react";
 import type { AppProps } from "next/app";
 import Layout from "@/components/molecules/Layout";
 import { useRouter } from "next/router";
+
 import "../src/styles/globals.css";
-const IGNORED_LAYOUT = ["/login" , "/createAccount"];
+
+const IGNORED_LAYOUT = ["/login", "/createAccount"];
+
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
   const withLayout = !IGNORED_LAYOUT.includes(router.pathname);
+
   return (
     <>
       {withLayout ? (
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </>
       ) : (
         <Component {...pageProps} />
       )}
