84cdc7693deea3b47d49905537be504721b8972c
Sahl Hafidz <akunbelajarhafidz@gmail.com>
2025-01-26 19:47:03 +0700

    feat(layout):testing deploy

--- pages/createAccount.tsx
+++ pages/createAccount.tsx
@@ -1,6 +1,12 @@
-import dynamic from 'next/dynamic';
+import dynamic from "next/dynamic";
 
-const Component = dynamic(() => import(/* webpackChunkName: "createAccount-page" */ '../src/components/templates/CreateAccount/index'), {
-  ssr: false,
-});
+const Component = dynamic(
+  () =>
+    import(
+      /* webpackChunkName: "createAccount-page" */ "@/components/templates/CreateAccount"
+    ),
+  {
+    ssr: false,
+  }
+);
 export default Component;
