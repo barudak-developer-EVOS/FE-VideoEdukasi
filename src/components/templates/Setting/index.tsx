912015d74a28adf2dd415bd9fb8b74d767b68771
Sahl Hafidz <akunbelajarhafidz@gmail.com>
2025-02-05 15:38:32 +0700

    feat(layout):adjust user-settings

--- src/components/templates/Setting/index.tsx
+++ src/components/templates/Setting/index.tsx
@@ -5,16 +5,17 @@ import { Button, Form, Input, Upload, message } from "antd";
 import { ArrowLeftOutlined, UploadOutlined } from "@ant-design/icons";
 import Image from "next/image";
 import axios from "axios";
-import Cookies from "js-cookie"; // Import js-cookie to access cookies
+import Cookies from "js-cookie";
 import { UploadFile } from "antd/es/upload/interface";
 
 const UserSettings: React.FC = () => {
   const router = useRouter();
-  const { id } = router.query; // Get ID from URL
+  const { id } = router.query; // Ambil ID dari URL
   const [form] = Form.useForm();
   const [initialValues, setInitialValues] = useState<any>(null);
   const [thumbnailPreview, setThumbnailPreview] = useState("");
   const [thumbnailFile, setThumbnailFile] = useState<File | null>(null);
+
   useEffect(() => {
     if (id) {
       const token = Cookies.get("token");
@@ -24,7 +25,6 @@ const UserSettings: React.FC = () => {
         return;
       }
 
-      // Proceed with fetching the data after token check
       axios
         .get(
           `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/accounts/get-accounts/${id}`,
@@ -49,7 +49,8 @@ const UserSettings: React.FC = () => {
           message.error("Failed to load account data.");
         });
     }
-  }, [id]); // Dependencies include `id` so that it updates when `id` changes
+  }, [id]);
+  // Dependencies include `id` so that it updates when `id` changes
 
   // Handle image upload and preview
 
@@ -57,18 +58,16 @@ const UserSettings: React.FC = () => {
     const url = URL.createObjectURL(file);
     setThumbnailPreview(url);
     setThumbnailFile(file);
-    return false; // Prevent automatic upload
+    return false; // Mencegah upload otomatis oleh Ant Design
   };
 
   // Handle form submission
   const handleSubmit = (values: any) => {
     if (id) {
-      // Retrieve token from cookies
       const token = Cookies.get("token");
 
       if (!token) {
         message.error("You are not authorized. Please log in.");
-        // router.push("/login"); // Redirect to login if no token is found
         return;
       }
 
@@ -79,38 +78,33 @@ const UserSettings: React.FC = () => {
       formData.append("name", values.name);
       formData.append("email", values.email);
 
-      // If an image is selected, append it to the form data
-
-      // Submit the form data to update the account information with Authorization header
       axios
         .put(
           `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/accounts/update-accounts/${id}`,
           formData,
           {
             headers: {
-              Authorization: `Bearer ${token}`, // Add the token to the Authorization header
-              "Content-Type": "multipart/form-data", // Ensure the form is sent as multipart
+              Authorization: `Bearer ${token}`,
+              "Content-Type": "multipart/form-data",
             },
           }
         )
         .then((response) => {
           if (response.status === 200) {
-            const profilePhotoUrl = response.data?.data?.profilePhoto; // Pastikan respons memiliki URL
-            console.log("Profile Photo URL:", profilePhotoUrl);
+            const profilePhotoUrl = response.data?.data?.profilePhoto;
             if (profilePhotoUrl) {
-              Cookies.set("profilePhoto", profilePhotoUrl, { path: "/" }); // Perbarui cookie dengan path root
+              Cookies.set("profilePhoto", profilePhotoUrl, { path: "/" });
             }
 
             message.success("Settings saved successfully!");
 
             router.push("/").then(() => {
-              window.location.reload(); // Lakukan reload setelah navigasi
-            }); // Go back to the previous page
+              window.location.reload(); // Reload setelah navigasi
+            });
           } else {
             message.error("Failed to save settings.");
           }
         })
-
         .catch((error) => {
           console.error("Error updating account data:", error);
           message.error("Failed to save settings.");
@@ -124,74 +118,69 @@ const UserSettings: React.FC = () => {
   }
 
   return (
-    <div style={{ padding: "20px", maxWidth: "600px", margin: "auto" }}>
-      {/* Back Button */}
-      <Button
-        icon={<ArrowLeftOutlined />}
-        onClick={() => router.back()}
-        style={{ marginBottom: "20px" }}
-      >
+    <div style={{ padding: "20px", maxWidth: "100%", margin: "auto" }}>
+      <Button type="primary" onClick={() => router.back()}>
+        <ArrowLeftOutlined />
         Back
       </Button>
+      <div style={{ padding: "20px", maxWidth: "600px", margin: "auto" }}>
+        <h1>User Settings</h1>
 
-      <h1>User Settings</h1>
-
-      <Form
-        form={form}
-        layout="vertical"
-        onFinish={handleSubmit}
-        initialValues={initialValues} // Use the fetched data as initial values
-      >
-        <Form.Item
-          label="Name"
-          name="name"
-          rules={[{ required: true, message: "Please enter your name" }]}
+        <Form
+          form={form}
+          layout="vertical"
+          onFinish={handleSubmit}
+          initialValues={initialValues}
         >
-          <Input placeholder="Enter your name" />
-        </Form.Item>
+          <div style={{ textAlign: "center" }}>
+            {thumbnailPreview && (
+              <Image
+                src={thumbnailPreview}
+                alt="Thumbnail Preview"
+                width={200}
+                height={200}
+                style={{
+                  marginTop: 10,
+                  objectFit: "cover",
+                  border: "1px solid #ccc",
+                  borderRadius: "50%", // Circular image
+                }}
+              />
+            )}
+            <Form.Item name="profilePhotoFile" style={{ marginTop: 20 }}>
+              <Upload
+                accept="image/*"
+                beforeUpload={handleThumbnailUpload}
+                showUploadList={false}
+              >
+                <Button icon={<UploadOutlined />}>Upload Photo Profile</Button>
+              </Upload>
+            </Form.Item>
+          </div>
 
-        <Form.Item
-          label="Email"
-          name="email"
-          rules={[{ required: true, message: "Please enter your email" }]}
-        >
-          <Input type="email" placeholder="Enter your email" />
-        </Form.Item>
-
-        <Form.Item
-          name="profilePhotoFile"
-          label="Upload Thumbnail"
-          rules={[{ required: true, message: "Please upload a thumbnail!" }]}
-          style={{ marginTop: 20 }}
-        >
-          <Upload
-            accept="image/*"
-            beforeUpload={handleThumbnailUpload}
-            showUploadList={false}
+          <Form.Item
+            label="Name"
+            name="name"
+            rules={[{ required: true, message: "Please enter your name" }]}
           >
-            <Button icon={<UploadOutlined />}>Upload Thumbnail</Button>
-          </Upload>
-        </Form.Item>
-        {thumbnailPreview && (
-          <Image
-            src={thumbnailPreview}
-            alt="Thumbnail Preview"
-            width={200}
-            height={120}
-            style={{
-              marginTop: 10,
-              objectFit: "cover",
-              border: "1px solid #ccc",
-            }}
-          />
-        )}
+            <Input placeholder="Enter your name" />
+          </Form.Item>
 
-        <Form.Item>
-          <Button type="primary" htmlType="submit" style={{ width: "100%" }}>
-            Save Settings
-          </Button>
-        </Form.Item>
-      </Form>
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[{ required: true, message: "Please enter your email" }]}
+          >
+            <Input type="email" placeholder="Enter your email" />
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" style={{ width: "100%" }}>
+              Save Settings
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
     </div>
   );
 };
